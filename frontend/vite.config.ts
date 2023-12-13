21ab2745acd555e827f69028fed91c660fc36e00
Nikita Yutanov <nikitayutanov@gmail.com>
2023-12-13 19:04:12 +0300

    Configure svg and absolute imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr(), checker({ typescript: true })],
+
+  resolve: { alias: { '@': '/src' } },
+});
