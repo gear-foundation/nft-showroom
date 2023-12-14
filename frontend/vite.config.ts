7478f22081af9ffd2fc92a7a9f1e3d08841d78b4
Nikita Yutanov <nikitayutanov@gmail.com>
2023-12-14 14:55:12 +0300

    Configure eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,14 @@ import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), checker({ typescript: true })],
+  plugins: [
+    react(),
+    svgr(),
+    checker({
+      typescript: true,
+      eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"', dev: { logLevel: ['error'] } },
+    }),
+  ],
 
   resolve: { alias: { '@': '/src' } },
 });
