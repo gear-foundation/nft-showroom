9762f366fb4e0c459faf028e52e6397b3ced8ac2
Nikita Yutanov <nikitayutanov@gmail.com>
2024-01-23 02:58:48 +0300

    Fix mint button display for collections with limit

--- frontend/src/features/collections/components/collection-header/collection-header.tsx
+++ frontend/src/features/collections/components/collection-header/collection-header.tsx
@@ -1,4 +1,5 @@
 import { HexString } from '@gear-js/api';
+import { withoutCommas } from '@gear-js/react-hooks';
 
 import { InfoCard } from '@/components';
 
@@ -49,6 +50,8 @@ function CollectionHeader({
       );
     });
 
+  const tokensToMint = tokensLimit ? +withoutCommas(tokensLimit) - tokensCount : 1;
+
   return (
     <header className={styles.container}>
       <div className={styles.header}>
@@ -70,7 +73,7 @@ function CollectionHeader({
         <div>
           <ul className={styles.socials}>{renderSocials()}</ul>
 
-          <MintNFT collectionId={id} value={mintPrice} />
+          {tokensToMint > 0 && <MintNFT collectionId={id} value={mintPrice} />}
         </div>
       </div>
     </header>
