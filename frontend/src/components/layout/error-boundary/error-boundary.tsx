0b4826fdc94da340fedeb45ec0ceb9395fc0a82a
Nikita Yutanov <nikitayutanov@gmail.com>
2024-02-17 00:12:27 +0300

    Add list component (#15)
    * Fix nested routes rerender
    
    * Add list component

--- frontend/src/components/layout/error-boundary/error-boundary.tsx
+++ frontend/src/components/layout/error-boundary/error-boundary.tsx
@@ -1,4 +1,7 @@
 import { Component, ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { useChangeEffect } from '@/hooks';
 
 import { BackButton } from '../../buttons';
 import { Container } from '../container';
@@ -9,9 +12,31 @@ type Props = {
   children: ReactNode;
 };
 
+type FallbackProps = {
+  message: string;
+  reset: () => void;
+};
+
 type State = {
   error: Error | null;
 };
+
+function Fallback({ message, reset }: FallbackProps) {
+  const { pathname } = useLocation();
+
+  useChangeEffect(() => {
+    reset();
+  }, [pathname]);
+
+  return (
+    <Container>
+      <h2 className={styles.heading}>Oops! Something went wrong:</h2>
+      <p className={styles.error}>{message}</p>
+
+      <BackButton size="small" />
+    </Container>
+  );
+}
 
 class ErrorBoundary extends Component<Props, State> {
   constructor(props: Props) {
@@ -23,17 +48,14 @@ class ErrorBoundary extends Component<Props, State> {
     return { error };
   }
 
+  reset = () => {
+    this.setState({ error: null });
+  };
+
   render() {
     if (!this.state.error) return this.props.children;
 
-    return (
-      <Container>
-        <h2 className={styles.heading}>Oops! Something went wrong:</h2>
-        <p className={styles.error}>{this.state.error.message}</p>
-
-        <BackButton size="small" />
-      </Container>
-    );
+    return <Fallback message={this.state.error.message} reset={this.reset} />;
   }
 }
 
