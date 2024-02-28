c68139cc01a7cdd8c6ce62165f43a75dcaf6510a
Nikita Yutanov <nikitayutanov@gmail.com>
2024-02-29 01:51:11 +0300

    Add create collection nfts limit

--- frontend/src/features/create-simple-collection/consts.ts
+++ frontend/src/features/create-simple-collection/consts.ts
@@ -38,9 +38,13 @@ const DEFAULT_NFTS_VALUES: NFTsValues = {
 const IMAGE_TYPES = ['image/svg+xml', 'image/png', 'image/jpeg'];
 const IMAGE_TYPES_ACCEPT = IMAGE_TYPES.join(', ');
 
-const MAX_SIZE_MB = {
-  IMAGE: 5,
-  NFTS_CHUNK: 50,
+const MAX = {
+  NFTS_COUNT: 10000,
+
+  SIZE_MB: {
+    IMAGE: 5,
+    NFTS_CHUNK: 50,
+  },
 };
 
 export {
@@ -51,5 +55,5 @@ export {
   DEFAULT_NFTS_VALUES,
   IMAGE_TYPES,
   IMAGE_TYPES_ACCEPT,
-  MAX_SIZE_MB,
+  MAX,
 };
