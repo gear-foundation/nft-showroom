893a1f79041c5f69c70b68e88427b103eb593b35
Aydar Farrakhov <stranniknm@gmail.com>
2024-02-29 18:17:58 +0300

    reduce pinata rate

--- pinata-proxy/src/pinata.service.ts
+++ pinata-proxy/src/pinata.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import PinataClient from '@pinata/sdk';
 import { RateLimiter } from 'limiter';
 
 @Injectable()
 export class PinataService {
+  private logger = new Logger(PinataService.name);
   private readonly pinata: PinataClient;
   private limiter: RateLimiter;
 
@@ -14,20 +15,23 @@ export class PinataService {
       'PINATA_SECRET_API_KEY',
     );
     this.limiter = new RateLimiter({
-      tokensPerInterval: 180,
+      tokensPerInterval: 150,
       interval: 'minute',
     });
     this.pinata = new PinataClient(apiKey, secretKey);
   }
 
   async uploadFile(file, fileName: string): Promise<string> {
+    this.logger.log(`start to uploading file ${fileName} to IPFS`);
     await this.limiter.removeTokens(1);
+    this.logger.log(`uploading file ${fileName} to IPFS`);
     const pinataInfo = await this.pinata.pinFileToIPFS(file, {
       pinataMetadata: {
         name: fileName,
       },
       pinataOptions: { cidVersion: 0 },
     });
+    this.logger.log(`file ${fileName} uploaded to IPFS`);
     return pinataInfo.IpfsHash;
   }
 }
