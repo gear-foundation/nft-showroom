086860f7e66abf21704c99fbea9fa97da237b766
Nikita Yutanov <nikitayutanov@gmail.com>
2024-03-29 20:28:54 +0300

    Add NFT page poll interval

--- frontend/src/pages/nft/hooks.ts
+++ frontend/src/pages/nft/hooks.ts
@@ -1,10 +1,10 @@
-import { useQuery, useSubscription } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 import { NFT_QUERY } from './consts';
 
 function useNFT(collectionId: string, idInCollection: string) {
   const id = `${collectionId}-${idInCollection}`;
-  const { data } = useQuery(NFT_QUERY, { variables: { id } });
+  const { data } = useQuery(NFT_QUERY, { variables: { id }, pollInterval: 10000 });
 
   return data?.nftById;
 }
