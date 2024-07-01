5ee1f641a3da77465999b713df5b85adbf918b64
Nikita Yutanov <nikitayutanov@gmail.com>
2024-07-01 18:48:20 +0300

    Add global nft search (#46)
    * Add global nft search
    
    * Rename feature
    
    * Fix legal footer links

---
+++ frontend/src/features/nft-search/hooks.ts
@@ -0,0 +1,24 @@
+import { useSearchParams } from 'react-router-dom';
+
+import { FIELD_NAME } from './consts';
+
+function useNFTSearchParam() {
+  const [params] = useSearchParams();
+  const value = params.get(FIELD_NAME.QUERY) || '';
+
+  const set = (query: string) => {
+    params.set(FIELD_NAME.QUERY, query);
+
+    return params.toString();
+  };
+
+  const reset = () => {
+    params.delete(FIELD_NAME.QUERY);
+
+    return '';
+  };
+
+  return { value, set, reset };
+}
+
+export { useNFTSearchParam };
