d1fd7a3b3beee82ca126af01de93404eff939e0d
Eugene Kotsarev <gimmethenick@gmail.com>
2025-08-05 20:33:36 +0300

    add: dynamic program id address

--- frontend/src/hooks/sails/api.ts
+++ frontend/src/hooks/sails/api.ts
@@ -1,12 +1,18 @@
 import { useProgram, useSendProgramTransaction } from '@gear-js/react-hooks';
+import { useQuery } from '@tanstack/react-query';
+import request from 'graphql-request';
 
-import { ADDRESS } from '@/consts';
-import { SailsProgram } from '@/hooks-sails/lib.ts';
+import { ADDRESS } from '@/consts.ts';
+import { MARKETPLACE_QUERY } from '@/context/marketplace/consts.ts';
+import { SailsProgram } from '@/hooks/sails/lib.ts';
 
 export function useProgramInstance() {
+  const { data } = useQuery({ queryKey: ['marketplace'], queryFn: () => request(ADDRESS.INDEXER, MARKETPLACE_QUERY) });
+  const programId = (data?.marketplaceById?.address || ADDRESS.CONTRACT) as typeof ADDRESS.CONTRACT;
+
   return useProgram({
     library: SailsProgram,
-    id: ADDRESS.CONTRACT,
+    id: programId,
   });
 }
 
